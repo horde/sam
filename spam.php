18229317dea6183471556af62db037c9285e0667
Ralf Lang <ralf.lang@ralf-lang.de>
2025-06-24 17:45:26 +0200

    fix: getInfo() no longer writes back to second parameter. Use return value.

--- spam.php
+++ spam.php
@@ -52,7 +52,7 @@ if ($form->isSubmitted() &&
 
 if ($form->validate($vars)) {
     $stackedOptions = array();
-    $form->getInfo($vars, $info);
+    $info = $form->getInfo($vars, $info);
     foreach (Sam::getAttributes() as $key => $attribute) {
         if ($sam_driver->hasCapability($key) && $vars->exists($key)) {
             $data = $info[$key];
